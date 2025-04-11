16298e05cc10b759c69662969b5af2a15f145870
Aakash Shinde <70742739+Aakashshinde99@users.noreply.github.com>
2025-04-11 21:35:27 +0530

    Update wsgi.py
    Creating superuser in wsgi.py

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -16,6 +16,7 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 import django
 from django.contrib.auth import get_user_model
 User = get_user_model()
+
 if not User.objects.filter(username='admin').exists():
     User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
 
