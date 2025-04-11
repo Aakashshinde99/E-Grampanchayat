040da11b99ec1512f2e7ac96a9a285d0c27538f4
Aakash Shinde <70742739+Aakashshinde99@users.noreply.github.com>
2025-04-11 19:38:09 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-import dj_database_url
-import os
-
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
