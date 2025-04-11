33a5dc369c89ab70c592fe89337de34296a3b25f
Aakash Shinde <70742739+Aakashshinde99@users.noreply.github.com>
2025-04-11 21:25:16 +0530

    Update wsgi.py

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -13,6 +13,10 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_project.settings')
 
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 'admin@gmail.com', 'admin123')
 
 application = get_wsgi_application()
