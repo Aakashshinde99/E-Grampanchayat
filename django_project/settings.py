ef997ac7c8af56965b59f0acbeaee9d36e2fcba6
Aakash Shinde <70742739+Aakashshinde99@users.noreply.github.com>
2025-04-23 23:30:46 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -201,6 +201,7 @@ CLOUDINARY_STORAGE = {
     'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
     'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
     'API_SECRET': os.getenv('CLOUDINARY_API_SECRET'),
+    'SECURE': True  # Force HTTPS
 }
 
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
